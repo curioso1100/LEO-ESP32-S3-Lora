b02f249dfb0692c60c9f2d3732a09f6899c2acfc
José García Pérez <curioso1100@yopmail.com>
2026-07-26 19:26:47 +0200

    Comienza V8.2

--- config_system.py
+++ config_system.py
@@ -18,7 +18,7 @@ import os
 # CONSTANTES (de configuracion.py)
 # -------------------------------------------------------------------------
 
-VERSION = "V8.1"
+VERSION = "V8.2"
 NOMBRE_PROYECTO = "LEO"
 
 _CONFIG_FILE = "config.json"
